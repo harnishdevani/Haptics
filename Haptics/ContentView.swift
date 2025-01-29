0f947b0788656775e45e16aace6c8e47b000eddc
harnishdevani <harnishdevani03@gmail.com>
2025-01-29 10:28:26 +0100

    Initial Commit

---
+++ Haptics/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Haptics
+//
+//  Created by Harnish Devani on 29/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Haptics/HapticsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HapticsApp.swift
+//  Haptics
+//
+//  Created by Harnish Devani on 29/01/25.
+//
+
+import SwiftUI
+
+@main
+struct HapticsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
